3488a37994389f08ff919555082450b7bd10855b
Aditya Gupta <adityaguptady@gmail.com>
2023-10-27 14:09:28 +0530

    delete functionality & completed functionality

--- appJS.js
+++ appJS.js
@@ -1,5 +1,25 @@
-const todo = []
+var todo = []
 var counter = 0
+
+function mockData()
+{
+    addtoArray(counter++, "Need to buy groceries", true)
+    addtoArray(counter++, "Need to complete assignment", false)
+    addtoArray(counter++, "Need to eat food", true)
+    addtoArray(counter++, "Need to sleep", false)
+    updateFrontend()
+    console.log(todo)
+}
+
+function addtoArray(id, text, completed)
+{
+    let tempTodo = {
+        id: id,
+        text: text,
+        completed: completed,
+    }
+    todo.push(tempTodo)
+}
 
 function addTodo(event)
 {
@@ -10,17 +30,13 @@ function addTodo(event)
     let tempTodo = {
         id: counter++,
         text: tempTodoText,
+        completed: false,
     }
     todo.push(tempTodo)
     console.log(todo)
     document.getElementById("todoText").value = ""
-
-    let todoList = document.getElementById("todoList")
-    todoList.innerHTML = ""
-    todo.forEach(element=>
-    {
-        todoList.innerHTML += "<li>"+element.text+"  <button onclick=\"deleteTodo("+element.id+")\">Delete</button></li>"
-    })
+    //updating Frontend
+    updateFrontend()
 }
 
 function deleteTodo(id)
@@ -28,6 +44,53 @@ function deleteTodo(id)
     console.log("------------------Delete todo------------------")
     console.log("id: "+id)
     todo = todo.filter(element=>{
+        //return true / false
+        return element.id != id
+    })
+    
+    updateFrontend()
+}
 
+function checkboxListener(id)
+{
+    console.log("id: "+id)
+    //console.log(todo)
+    todo = todo.map(element=>
+        {
+            if(element.id == id)
+            {
+                    element.completed = !element.completed
+            }
+            return element
+        })
+    console.log(todo)
+    updateFrontend()
+}
+
+function updateFrontend()
+{
+    //updating Frontend
+    let todoList = document.getElementById("todoList")
+    todoList.innerHTML = ""
+    todo.forEach(element=>
+    {
+        if(element.completed)
+        {
+            todoList.innerHTML += 
+                "<li>"+
+                    "<input type=\"checkbox\" checked onclick=\"checkboxListener("+element.id+")\"></input><s>"+
+                    element.text+
+                    "</s>  <button onclick=\"deleteTodo("+element.id+")\">Delete</button>"+
+                "</li>"
+        }
+        else
+        {
+            todoList.innerHTML += 
+                "<li>"+
+                    "<input type=\"checkbox\"  onclick=\"checkboxListener("+element.id+")\"></input>"+
+                    element.text+
+                    "  <button onclick=\"deleteTodo("+element.id+")\">Delete</button>"+
+                "</li>"
+        }
     })
 }
